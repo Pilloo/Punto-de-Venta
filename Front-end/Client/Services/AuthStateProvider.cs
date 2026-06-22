522b5d5a10bba4ff2a089da1ae1aba6ab79f6909
Andrés Chacón <andreschaconmora3@gmail.com>
2026-06-21 18:09:29 -0600

    i forgot to add this as well

---
+++ Front-end/Client/Services/AuthStateProvider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Client.Services
+{
+    public class AuthStateProvider(LocalStorageService localStorage) : AuthenticationStateProvider
+    {
+        private ClaimsPrincipal _anonymous = new(new ClaimsIdentity());
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+
+        }
+    }
+}
